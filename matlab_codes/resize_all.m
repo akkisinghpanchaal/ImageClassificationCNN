9944ac3acf945d3ee4c36fcceb8740915867de8d
akkisingpanchaal <akkisinghpanchaal@gmail.com>
2017-04-25 16:34:02 +0530

    added resized images of the feature regions, added resizing code

---
+++ matlab_codes/resize_all.m
@@ -0,0 +1,11 @@
+dirData = dir('../CNN_data/Mouth/*.jpg'); %takes all the images from the folder/ you can also assign specific address of the folder
+fileNames = {dirData.name};
+for k = 1:length(dirData) %the loop will continue for the number of images
+    filename = dirData(k).name
+    data1 = imread(strcat('../CNN_data/Mouth/',filename));
+    data2 = imresize(data1,[96 96]); %w=40, h=30
+    iFile = filename;
+    newName = sprintf('.jpg',iFile);
+        imwrite(data2,['../resized_CNN_data/Mouth/' filename, newName,]);
+     
+   end
